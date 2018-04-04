0bdd992ab2b62fdb1a753c182ff39a90515d3166
Lizaveta <100317944@alumnos.uc3m.es>
2018-04-04 15:42:59 +0200

    Created inscription, notification and favorites pages

--- eventosUC3M/src/app/app.module.ts
+++ eventosUC3M/src/app/app.module.ts
@@ -5,6 +5,9 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
+import {InscriptionsPage} from '../pages/inscriptions/inscriptions';
+import {FavoritesPage} from '../pages/favorites/favorites';
+import {NotificationsPage} from '../pages/notifications/notifications';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
