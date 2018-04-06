31f4bd371a19a9c5dcb38cfe615c4f801aa86dd6
Lizaveta <100317944@alumnos.uc3m.es>
2018-04-06 16:00:14 +0200

    Connected notification config to user

--- eventosUC3M/src/pages/config-notifications/config-notifications.ts
+++ eventosUC3M/src/pages/config-notifications/config-notifications.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import {User} from '../../models/user.model';
+import {USER} from '../../mock-user';
 /**
  * Generated class for the ConfigNotificationsPage page.
  *
@@ -14,7 +15,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'config-notifications.html',
 })
 export class ConfigNotificationsPage {
-
+  user: User = USER;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
